573becda4e62e2a40327e37279ea892dad592d98
RipNasty <det625@gmail.com>
2023-11-10 17:02:47 -0500

    try directly importing

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
+# app.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Import Flask-Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://davidthomas@localhost/herosmith_development'
@@ -7,10 +9,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Initialize Flask-Migrate
+migrate = Migrate(app, db)
+
 @app.route('/')
 def hello_world():
     return 'Hello, HeroSmith!'
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
