392f1290effe78ffab17f81aeb3a7fd6ef4f8ced
RipNasty <det625@gmail.com>
2023-11-10 16:50:19 -0500

    fixed db line

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://davidthomas@localhost/herosmith_development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, HeroSmith!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
