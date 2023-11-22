a18568339d36e6d9f91d705a453c78c4df4d78c1
RipNasty <det625@gmail.com>
2023-11-22 10:00:49 -0500

    user model made

---
+++ backend/models/user.py
@@ -0,0 +1,17 @@
+# backend/models/user.py
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    characters = db.relationship('Character', backref='user', lazy=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
