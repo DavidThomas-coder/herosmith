0b2d0f9dccce5830e86471c0bc94ea389bff7fcb
RipNasty <det625@gmail.com>
2023-11-22 15:18:16 -0500

    finally works

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,6 +1,7 @@
 from logging.config import fileConfig
 
 from models.user import User
+from models.character import Character
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -21,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [User.metadata]
+target_metadata = [User.metadata, Character.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
