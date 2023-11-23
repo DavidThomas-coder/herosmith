b28ce5ef818c0ed3d482f65caf0cdc8bf6bd1fd4
RipNasty <det625@gmail.com>
2023-11-22 20:43:23 -0500

    index page made

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to HeroSmith!</h1>
+      <p>Build your characters and embark on epic adventures.</p>
+    </div>
+  );
+};
+
+export default Home;
