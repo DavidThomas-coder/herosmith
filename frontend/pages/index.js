113d1e70e39ed57655513545a9387f9662ae9179
RipNasty <det625@gmail.com>
2024-01-11 16:29:04 -0500

    index.js updated

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,22 @@
-// pages/index.js
+
+// frontend/pages/index.js
 
 import React from 'react';
+import CreateCharacterForm from '../components/CreateCharacterForm';
+import SignUpForm from '../components/SignUpForm';
+import LoginForm from '../components/LoginForm';
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to HeroSmith</h1>
       <p>Build your characters and embark on epic adventures!</p>
+      <CreateCharacterForm />
+      <SignUpForm />
+      <LoginForm />
     </div>
   );
 };
 
 export default Home;
+
